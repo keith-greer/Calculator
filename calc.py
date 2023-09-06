2b13886b102060c292e9b0582af9bc219f8705bc
keith-greer <keithpgreer@gmail.com>
2023-09-06 11:34:54 +0100

    First commit

---
+++ calc.py
@@ -0,0 +1,13 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+addition_result = num1 + num2
+subtraction_result = num1 - num2
+multiplication_result = num1 * num2
+division_result = num1 / num2
+
+print("Addition result:", addition_result)
+print("Subtraction result:", subtraction_result)
+print("Multiplication result", multiplication_result)
+print("Division result", division_result)
+print("finish")
